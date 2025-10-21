32f6e83c87a4d2e7955132a0832ed6cb960d6104
Simon Droscher <simon@droscher.com>
2025-10-20 17:31:21 -0700

    Remove `api` from path in main dart file

--- lib/beergargoyle_client.dart
+++ lib/beergargoyle_client.dart
@@ -1,9 +1,9 @@
 library beergargoyle_client;
 
 // Export all generated protobuf files
-export 'api/v1/beer.pb.dart';
-export 'api/v1/beer.pbgrpc.dart';
-export 'api/v1/cellar.pb.dart';
-export 'api/v1/cellar.pbgrpc.dart';
-export 'api/v1/user.pb.dart';
-export 'api/v1/user.pbgrpc.dart';
+export 'v1/beer.pb.dart';
+export 'v1/beer.pbgrpc.dart';
+export 'v1/cellar.pb.dart';
+export 'v1/cellar.pbgrpc.dart';
+export 'v1/user.pb.dart';
+export 'v1/user.pbgrpc.dart';
